c82211030a42b2e09f91210eb9416a9308f9ec87
brenneralexandria <obrenneralexandria@gmail.com>
2023-06-14 10:37:05 -0300

    Adicionando dependencias maven e spring boot

---
+++ src/main/java/com/desenvolvimentoapi/apigerenciadordecompras/ApiGerenciadorDeComprasApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimentoapi.apigerenciadordecompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorDeComprasApplication.class, args);
+	}
+
+}
